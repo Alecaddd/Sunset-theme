ca9582ec841fe40a9caae1388cdbc0c7a40d40ef
Alecaddd <castellani.ale@gmail.com>
2015-12-03 19:57:04 -0800

    Lesson 8 and 9 updated

---
+++ Lesson 8-9/functions.php
@@ -0,0 +1,5 @@
+<?php
+	
+require get_template_directory() . '/inc/function-admin.php';
+require get_template_directory() . '/inc/enqueue.php';
+require get_template_directory() . '/inc/theme-support.php';
