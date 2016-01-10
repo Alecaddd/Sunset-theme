87d17570f3924bc8af006e3ceb68a7b870bcb4cd
Alecaddd <castellani.ale@gmail.com>
2016-01-10 15:43:50 -0800

    Lesson 17

---
+++ Lesson 17/functions.php
@@ -0,0 +1,7 @@
+<?php
+	
+require get_template_directory() . '/inc/cleanup.php';
+require get_template_directory() . '/inc/function-admin.php';
+require get_template_directory() . '/inc/enqueue.php';
+require get_template_directory() . '/inc/theme-support.php';
+require get_template_directory() . '/inc/custom-post-type.php';
---
+++ Lesson 17/inc/theme-support.php
@@ -0,0 +1,54 @@
+<?php
+
+/*
+	
+@package sunsettheme
+	
+	========================
+		THEME SUPPORT OPTIONS
+	========================
+*/
+
+$options = get_option( 'post_formats' );
+$formats = array( 'aside', 'gallery', 'link', 'image', 'quote', 'status', 'video', 'audio', 'chat' );
+$output = array();
+foreach ( $formats as $format ){
+	$output[] = ( @$options[$format] == 1 ? $format : '' );
+}
+if( !empty( $options ) ){
+	add_theme_support( 'post-formats', $output );
+}
+
+$header = get_option( 'custom_header' );
+if( @$header == 1 ){
+	add_theme_support( 'custom-header' );
+}
+
+$background = get_option( 'custom_background' );
+if( @$background == 1 ){
+	add_theme_support( 'custom-background' );
+}
+
+/* Activate Nav Menu Option */
+function sunset_register_nav_menu() {
+	register_nav_menu( 'primary', 'Header Navigation Menu' );
+}
+add_action( 'after_setup_theme', 'sunset_register_nav_menu' );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
