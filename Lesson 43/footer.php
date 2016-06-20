c710b9547a1eddad73f82b1a515e9b6e4c5ea37d
Alecaddd <castellani.ale@gmail.com>
2016-06-19 17:05:10 -0700

    Lesson 43

---
+++ Lesson 43/footer.php
@@ -0,0 +1,15 @@
+<?php 
+	
+	/*
+		This is the template for the footer
+		
+		@package sunsettheme
+	*/
+	
+?>
+
+<footer class="sunset-footer text-center">This is the footer</footer>
+
+<?php wp_footer(); ?>
+</body>
+</html>
